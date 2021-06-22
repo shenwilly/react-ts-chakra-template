a753fe1c058326a5b160a1716fd304ab296a9e75
willy <shen.willy95@gmail.com>
2021-06-22 14:58:20 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
+import { Container, ChakraProvider } from "@chakra-ui/react"
+import styled, { ThemeProvider } from "styled-components"
+
+import Home from "./pages/Home"
+import Header from "./components/Header"
+import chakraTheme from "./utils/chakraTheme"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <SiteWrapper>
+        <Header />
+        <BodyWrapper maxW="container.xl" display="flex" alignItems="start" justifyContent="center">
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </BodyWrapper>
+      </SiteWrapper>
+    </Router>
   );
 }
 
-export default App;
+const SiteWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow-y: scroll;
+`;
+
+const BodyWrapper = styled(Container)`
+  height: ${props => `calc(100vh - ${props.theme.headerHeight})`}
+`;
+
+const Providers: React.FC = ({ children }) => {
+  const styledTheme = {
+    headerHeight: '60px',
+  };
+
+  return (
+    <ThemeProvider theme={styledTheme}>
+      <ChakraProvider theme={chakraTheme}>
+        {children}
+      </ChakraProvider>
+    </ThemeProvider>
+  );
+};
+
+function withProviders<P>(
+  Component: React.ComponentType<P>
+) {
+  const ComponentProviders = (props: P) => {
+    return (
+      <Providers>
+        <Component {...props}/>
+      </Providers>
+    )
+  };
+  return ComponentProviders;
+}
+
+export default withProviders(App);
